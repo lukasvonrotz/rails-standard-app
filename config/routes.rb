8e990aea48372dbdb5df12252b94a0d5579e22c9
Lukas von Rotz <lukas.vonrotz@prola.ch>
2017-07-21 18:36:57 +0200

    everything styled with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions'}
+  devise_for :users, controllers: { confirmations: 'users/confirmations',
+                                    mailer: 'users/mailer',
+                                    passwords: 'users/passwords',
+                                    registrations: 'users/registrations',
+                                    sessions: 'users/sessions',
+                                    shared: 'users/shared',
+                                    unlocks: 'users/unlocks'}
   get 'home/index'
 
   root 'home#index'
